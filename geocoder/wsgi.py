ae524f4f2b2bc644fe6987678534246e7007fe43
Matt Westcott <matt@west.co.tt>
2016-04-18 21:34:38 +0100

    ohai I found the secret settings config line

--- geocoder/wsgi.py
+++ geocoder/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "geocoder.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "geocoder.settings.production")
 
 application = get_wsgi_application()
